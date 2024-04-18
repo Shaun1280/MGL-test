f6de721e518085363491e6fadb32f1815aa2b98f
Shaun1280 <jpanau@connect.ust.hk>
2024-04-18 16:50:06 +0800

    add encoder map generation

--- data_process2.py
+++ data_process2.py
@@ -7,58 +7,9 @@ from copy import deepcopy
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 
-core = cpu_count()
 
-def parallelize(data, func, num_of_processes=core):
-    data_split = np.array_split(data, num_of_processes)
-    with Pool(num_of_processes) as pool:
-        data_list = pool.map(func, data_split)
-    data = pd.concat(data_list)
-    return data
-
-
-def run_on_subset(func, data_subset):
-    return data_subset.apply(func, axis=1)
-
-
-def parallelize_on_rows(data, func, num_of_processes=core):
-    return parallelize(data, partial(run_on_subset, func), num_of_processes)
-
-def get_item_count(item_list, row):
-    item_count = item_list.count(row.itemid)
-    return item_count
-
-def get_all_item_counts(data, item_list):
-    item_counts = parallelize_on_rows(data, partial(get_item_count, deepcopy(item_list)))
-    return item_counts
-
-def get_user_count(user_list, row):
-    user_count = user_list.count(row.userid)
-    return user_count
-
-def get_all_user_counts(data, user_list):
-    user_counts = parallelize_on_rows(data, partial(get_user_count, deepcopy(user_list)))
-    return user_counts
-
-def get_user_keep(history_users, row):
-    return (row.userid in history_users)
-
-def get_all_user_keeps(data, history_users):
-    user_keeps = parallelize_on_rows(data, partial(get_user_keep, deepcopy(history_users)))
-    return user_keeps
-
-
-
-def get_item_keep(history_items, row):
-    return (row.itemid in history_items)
-
-def get_all_item_keeps(data, history_items):
-    item_keeps = parallelize_on_rows(data, partial(get_item_keep, deepcopy(history_items)))
-    return item_keeps
-
-
-def data_process(dataset_name, split_rate=0.9, user_fre_threshold=None,
-                 item_fre_threshold=None, shuffle_split=True, with_time=False, leave_out=None):
+def data_process(dataset_name, split_rate=0.9, user_freq_threshold=None,
+                 item_freq_threshold=None, shuffle_split=True, with_time=False, leave_out=None):
     save_dir = os.path.join(os.path.dirname(__file__), "dataset", dataset_name)
 
     if not os.path.exists(save_dir):
@@ -72,11 +23,56 @@ def data_process(dataset_name, split_rate=0.9, user_fre_threshold=None,
         names= ['userid', 'itemid', 'score', 'timestamp']
     )
 
-    print(interact.head())
+    # Encode user and item ids
+    if with_time == True:
+        interact = interact.sort_values("timestamp")
+    
+    # Filter out users that do not meet the frequency threshold
+    if user_freq_threshold is not None:
+        user_counts = interact['userid'].value_counts()
+        interact['user_count'] = interact['userid'].apply(lambda x: user_counts[x])
+        interact = interact[interact['user_count'] > user_freq_threshold]
+        interact.drop(columns=['user_count'], inplace=True)
+
+    # Filter out items that do not meet the frequency threshold
+    if item_freq_threshold is not None:
+        item_counts = interact['itemid'].value_counts()
+        interact['item_count'] = interact['itemid'].apply(lambda x: item_counts[x])
+        interact = interact[interact['item_count'] > item_freq_threshold]
+        interact.drop(columns=['item_count'], inplace=True)
+
+    # user id label encoder 
+    user_id_encoder = LabelEncoder()
+    user_id_encoder.fit(interact['userid'])
+    interact['userid'] = user_id_encoder.transform(interact['userid'])
+    
+    # save user encoder map
+    user_encoder_map = pd.DataFrame({
+        'encoded': np.arange(len(user_id_encoder.classes_)),
+        'user': user_id_encoder.classes_,
+    })
+    user_encoder_map.to_csv(os.path.join(save_dir, 'user_encoder_map.csv'), index=False)
+
+    # item id label encoder
+    item_id_encoder = LabelEncoder()
+    item_id_encoder.fit(interact['itemid'])
+    interact['itemid'] = item_id_encoder.transform(interact['itemid']) 
+    
+    # save item encoder map
+    item_encoder_map = pd.DataFrame({
+        'encoded': np.arange(len(item_id_encoder.classes_)),
+        'item': item_id_encoder.classes_,
+    })
+    item_encoder_map.to_csv(os.path.join(save_dir, 'item_encoder_map.csv'), index=False)
+    
+    # print(interact.head(20))
+    # print(interact.describe())
 
     
 
 if __name__ == '__main__':
-    data_process('bx');
+    data_process('bx',
+                 user_freq_threshold=None,
+                 item_freq_threshold=None);
 
 
