969ecf37203ea57e3c50e99c167f0c53049e068b
Shaun1280 <jpanau@connect.ust.hk>
2024-04-18 17:42:48 +0800

    update

--- data_process2.py
+++ data_process2.py
@@ -49,9 +49,19 @@ def data_process(dataset_name, split_rate=0.9, user_freq_threshold=None,
     # save user encoder map
     user_encoder_map = pd.DataFrame({
         'encoded': np.arange(len(user_id_encoder.classes_)),
-        'user': user_id_encoder.classes_,
+        'user': user_id_encoder.classes_.astype(str),
     })
     user_encoder_map.to_csv(os.path.join(save_dir, 'user_encoder_map.csv'), index=False)
+
+    # save encoded user feature
+    user_feature = pd.read_pickle(os.path.join(save_dir, 'user_feature.pkl'))
+    user_feature['user'] = user_feature['user'].astype(str)
+    user_feature = pd.merge(
+        user_encoder_map,
+        user_feature,
+        on='user'
+    )
+    user_feature.to_pickle(os.path.join(save_dir, 'encoded_user_feature.pkl'))
 
     # item id label encoder
     item_id_encoder = LabelEncoder()
@@ -65,14 +75,54 @@ def data_process(dataset_name, split_rate=0.9, user_freq_threshold=None,
     })
     item_encoder_map.to_csv(os.path.join(save_dir, 'item_encoder_map.csv'), index=False)
     
-    # print(interact.head(20))
-    # print(interact.describe())
+    # save encoded item feature
+    item_feature = pd.read_pickle(os.path.join(save_dir, 'item_feature.pkl'))
+    item_feature['item'] = item_feature['item'].astype(str)
+    item_feature = pd.merge(
+        item_encoder_map,
+        item_feature,
+        on='item'
+    )
+    item_feature.to_pickle(os.path.join(save_dir, 'encoded_item_feature.pkl'))
 
+    # Split the data into train and test sets
+    if leave_out == None:
+        interact_train, interact_test = \
+            train_test_split(interact, train_size=split_rate, random_state=5, shuffle=shuffle_split)
+    else:
+        # Leave out the last 'leave_out' interactions for each user
+        interact_chunk = list(interact.groupby('userid'))
+        interact_train = [chunk[1][:-leave_out] if len(chunk[1]) > leave_out else chunk[1] for chunk in interact_chunk]
+        interact_test = [chunk[1][-leave_out:] if len(chunk[1]) > leave_out else pd.DataFrame() for chunk in interact_chunk]
+        interact_train = pd.concat(interact_train, ignore_index=True)
+        interact_test = pd.concat(interact_test, ignore_index=True)
+
+    # get all user keeps
+    def get_all_user_keeps(interact_test, history_users):
+        user_keeps = interact_test['userid'].apply(lambda x: x in history_users)
+        return user_keeps
+
+    # get all item keeps
+    def get_all_item_keeps(interact_test, history_items):
+        item_keeps = interact_test['itemid'].apply(lambda x: x in history_items)
+        return item_keeps
     
+    # Filter out users and items that do not appear in the training set
+    history_users = set(interact_train['userid'])
+    history_items = set(interact_train['itemid'])
+    user_keeps = get_all_user_keeps(interact_test, history_users)
+    item_keeps = get_all_item_keeps(interact_test, history_items)
+    interact_test = interact_test[user_keeps & item_keeps]
+
+    # Save the train and test sets
+    interact_train.to_pickle(os.path.join(save_dir, "interact_train.pkl"))
+    interact_test.to_pickle(os.path.join(save_dir, "interact_test.pkl"))
 
 if __name__ == '__main__':
     data_process('bx',
                  user_freq_threshold=None,
-                 item_freq_threshold=None);
+                 item_freq_threshold=None,
+                 leave_out=1
+                );
 
 
