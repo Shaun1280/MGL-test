48d7819cd0f16bfd87e24c7b22ec3a2aa03806dd
Shane <143796207+Shane0821@users.noreply.github.com>
2024-04-26 21:28:38 +0800

    remove compute embeddings

--- model2.py
+++ model2.py
@@ -325,29 +325,3 @@ class Model(nn.Module):
         score = torch.mm(user_embedded, pos_item_embedded.t())
 
         return score
-
-    def compute_embeddings(self):
-        row_index, colomn_index, joint_enhanced_value = self.link_predict(self.item_degrees, self.top_rate)
-        indice = torch.cat([row_index, colomn_index], dim=0).to(self.device)
-
-        # user_embeddings, item_embeddings = self.compute_embeddings()
-        cur_embedding = torch.cat([self.user_id_Embeddings.weight, self.item_id_Embeddings.weight], dim=0)
-
-        all_embeddings = [cur_embedding]
-
-        # item_num
-        enhance_weight = torch.from_numpy(inverse_sigmoid(self.item_degree_numpy, self.convergence))
-        # user_num + item_num
-        enhance_weight = torch.cat([torch.zeros(self.user_num), enhance_weight], dim=-1).to(self.device).float()
-
-        for i in range(self.L):
-            cur_embedding_ori = torch.mm(self.adjacency_matrix_normed, cur_embedding)
-            cur_embedding_enhanced = torch_sparse.spmm(indice, joint_enhanced_value, self.user_num + self.item_num, self.user_num + self.item_num, cur_embedding)
-            cur_embedding = cur_embedding_ori + enhance_weight.unsqueeze(-1) * cur_embedding_enhanced
-            all_embeddings.append(cur_embedding)
-
-        all_embeddings = torch.stack(all_embeddings, dim=0)
-        all_embeddings = torch.mean(all_embeddings, dim=0, keepdim=False)
-        user_embeddings, item_embeddings = torch.split(all_embeddings, [self.user_num,self.item_num])
-
-        return user_embeddings, item_embeddings
