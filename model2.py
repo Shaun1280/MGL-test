15a63bbec5b92c65db85fc403294e326789d6eed
Shane <143796207+Shane0821@users.noreply.github.com>
2024-04-27 10:58:23 +0800

    upd pcl loss

--- model2.py
+++ model2.py
@@ -144,14 +144,16 @@ class Model(nn.Module):
         item_pop = pop(item_degree, self.convergence)
         
         # this determines whether the pcl loss will be omitted
-        m = torch.distributions.binomial.Binomial(1, torch.from_numpy(item_pop)).sample().to(self.device)
+        keep = torch.distributions.binomial.Binomial(1, torch.from_numpy(item_pop)).sample().to(self.device)
 
         l_pcl = functional.mse_loss(observed_item_org_embedding, self.item_id_Embeddings(observed_item), reduction='none').mean(dim=-1, keepdim=False) 
         
-        if m.sum().item() == 0:
-            return 0 * torch.mul(m, l_pcl).sum()
+        term_count = keep.sum()
+
+        if term_count.item() == 0:
+            return 0 * torch.mul(keep, l_pcl).sum()
         
-        l_pcl = torch.mul(m, l_pcl).sum() / m.sum() # taking the average over a batch
+        l_pcl = torch.mul(keep, l_pcl).sum() / term_count # taking the average over a batch
         return l_pcl
         
 
